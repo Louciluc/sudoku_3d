7da6a5c2f560ff97706bf9ad7bf5dd54b212f64c
Louci <lucasbuschendorf22102000@gmail.com>
2026-03-14 14:39:50 +0100

    Second commit, get_remaining function is wwork in progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,32 @@
-pub fn solve_sudoku(&mut sdk : [[[Option<usize>]]]) -> [[[usize]]]{
-    
+pub fn solve_sudoku( sdk : Vec<Vec<Vec<Option<usize>>>>) -> Vec<Vec<Vec<Option<usize>>>>{
+    // return 3-dimensional vector of int
+    sdk
 }
 
-fn get_remaining(&sdk : [[[Option<usize>]]], &pos : (usize, usize, usize)) -> [usize]{
+fn get_remaining(sdk : &Vec<Vec<Vec<Option<usize>>>>, pos : &(usize, usize, usize)) -> Vec<usize>{
     // Size should be the same in all dimensions
-    sdk_size = sdk.len();
+    let sdk_size = sdk.len();
 
-    all_values = vec![1..sdk_size];
-    print!(all_values);
+    let mut remain_values : Vec<usize> = (1..sdk_size).map(usize::from).collect(); // turn it from range<usize> to usize 
+    print!("{:?}", remain_values);
 
     // loop through each dimension and remove all occuring numbers
-    
+    //let sdk_iter = sdk.iter();
+    for y in 0..sdk_size{
+        for z in 0..sdk_size{
+            match sdk[pos.0][y][z]{
+                Some(val) => remain_values.retain(|&x| x != val),
+                None => {}
+            }
+        }
+    }
+
+    print!("Remaining values at positon {:?}: {:?}", pos,  remain_values);
+    // loop in the current box
+    // TODO
+
+
+    return remain_values;
 }
 
 /* 
